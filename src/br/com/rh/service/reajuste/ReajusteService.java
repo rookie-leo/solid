e3426f8daeb52719a20db46291daea945e8c6c92
rookie-leo <proleonarpa@gmail.com>
2022-05-11 15:30:00 -0300

    Correção após implementação do LSP

--- src/br/com/rh/service/reajuste/ReajusteService.java
+++ src/br/com/rh/service/reajuste/ReajusteService.java
@@ -17,7 +17,7 @@ public class ReajusteService {
 		this.validacoes.forEach(v -> v.validar(funcionario, aumento));
 
 		
-		BigDecimal salarioReajustado = funcionario.getSalario().add(aumento);
+		BigDecimal salarioReajustado = funcionario.getDadosPessoais().getSalario().add(aumento);
 		funcionario.salarioAtualizado(salarioReajustado);
 	}
 	
